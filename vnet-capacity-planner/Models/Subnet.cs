88060ae50f229f271a1d41d64dda8c75019d34d5
Chun Liu <chunliu@microsoft.com>
2020-12-09 17:04:12 +0800

    enhance validation

--- vnet-capacity-planner/Models/Subnet.cs
+++ vnet-capacity-planner/Models/Subnet.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 
 namespace vnet_capacity_planner.Models
 {
@@ -16,14 +17,12 @@ namespace vnet_capacity_planner.Models
         [Required]
         [IPAddress]
         public string StartIP { get; set; }
-
         [Required]
         public string ServiceName { get; set; }
         [Required]
         public int ServiceInstances { get; set; }
         public int IpPerInstance { get; set; }
         public int AdditionalIps { get; set; }
-
         [DisplayName("Address Range")]
         public string AddressRange 
         { 
@@ -33,22 +32,16 @@ namespace vnet_capacity_planner.Models
                 return network == null ? string.Empty : $"{network.Network} - {network.Broadcast}"; 
             }
         }
-
         [DisplayName("Address Count")]
         public int AddressCount => (int)(Network?.Total ?? 0);
-
         [DisplayName("Available Count")]
         public int AvailableCount => Service?.FixedSubnetCidr ?? false ? 0 : AddressCount - ((ServiceInstances * IpPerInstance) + AdditionalIps + ReservedIps);
         public int UsedCount => Service?.FixedSubnetCidr ?? false ? AddressCount : (ServiceInstances * IpPerInstance) + AdditionalIps + ReservedIps;
         [DisplayName("Address Space")]
         public string AddressSpace => Network?.ToString() ?? string.Empty;
-
         public VirtualNetwork VirtualNetwork { get; set; }
-
         public ServiceSpec Service { get; set; }
-
         public IPNetwork Network => PopulateNetwork();
-
         public IPNetwork PopulateNetwork()
         {
             if (Service == null)
@@ -90,30 +83,34 @@ namespace vnet_capacity_planner.Models
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             var results = new List<ValidationResult>();
-
+            // Subnet name must be unique.
             if (VirtualNetwork.Subnets.Where(s => s.Name.Equals(Name)).FirstOrDefault() != null)
             {
                 results.Add(new ValidationResult("Subnet name must be unique within a virtual network."));
             }
-
+            // IpPerInstance must be > 0 for non fixed cidr.
             if (!Service.FixedSubnetCidr && IpPerInstance <= 0)
             {
-                results.Add(new ValidationResult("IP/Instance must be greater than 0."));
+                results.Add(new ValidationResult("Address/Instance must be greater than 0."));
             }
-
+            // Number of instances need to be between min and max.
             if (!Service.FixedSubnetCidr && Service.MaxInstances > Service.MinInstances
                 && (ServiceInstances > Service.MaxInstances || ServiceInstances < Service.MinInstances))
             {
                 results.Add(new ValidationResult($"The instance must be between {Service.MinInstances} and {Service.MaxInstances}."));
             }
-
+            
             var network = Network;
+            // Validate CIDR block.
             if (!Equals(network.Network, IPAddress.Parse(StartIP)))
             {
-                results.Add(new ValidationResult($"{StartIP}/{network.Cidr} is not a valid CIDR block."));
+                var current = IPNetwork.ToBigInteger(network.Network);
+                var next = current + network.Total;
+                var nextIp = IPNetwork.ToIPAddress(next, AddressFamily.InterNetwork);
+                results.Add(new ValidationResult($"{StartIP}/{network.Cidr} is not a valid CIDR block. Try {network.Network} or {nextIp} instead."));
                 return results;
             }
-
+            // Validate if the subnet is in the vnet range.
             bool wideResult = IPNetwork.TryWideSubnet(
                 new IPNetwork[]
                 {
@@ -125,7 +122,7 @@ namespace vnet_capacity_planner.Models
                 results.Add(new ValidationResult($"The subnet address range {network} is not contained in the virtual network's address spaces."));
                 return results;
             }
-
+            // Overlap with other subnets
             foreach (var subnet in VirtualNetwork.Subnets)
             {
                 if(network.Overlap(subnet.Network))
