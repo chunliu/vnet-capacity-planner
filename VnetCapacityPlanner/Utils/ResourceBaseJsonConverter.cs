21736cb0a8607ff2077040a8c0af17f629994993
Chun Liu <chunliu@microsoft.com>
2022-04-22 21:18:22 +0800

    Simplify the code

--- VnetCapacityPlanner/Utils/ResourceBaseJsonConverter.cs
+++ VnetCapacityPlanner/Utils/ResourceBaseJsonConverter.cs
@@ -8,16 +8,9 @@ namespace VnetCapacityPlanner.Utils
     public class ResourceBaseJsonConverter : JsonConverter<ResourceBase>
     {
         public override ResourceBase Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            throw new NotImplementedException();
-        }
+            => throw new NotImplementedException();
 
         public override void Write(Utf8JsonWriter writer, ResourceBase value, JsonSerializerOptions options)
-        {
-            var itemType = value.GetType();
-
-            // pass on to default serializer
-            JsonSerializer.Serialize(writer, value, itemType, options);
-        }
+            => JsonSerializer.Serialize(writer, value, value.GetType(), options);
     }
 }
